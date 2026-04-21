0f730f3dfb4e03d672e4d2160fbb11bdb351db8b
jx2924-ux <jx2924@nyu.edu>
2026-04-21 17:37:46 +0800

    Delete Gomoku directory

--- Gomoku/Point.py
+++
@@ -1,7 +0,0 @@
-class Point:
-
-    def __init__(self, x, y):
-        self.x = x;
-        self.y = y;
-        self.pixel_x = 30 + 30 * self.x
-        self.pixel_y = 30 + 30 * self.y
--- Gomoku/main.py
+++
@@ -1,8 +0,0 @@
-import GUI
-import tkinter
-
-if __name__ == '__main__':
-    window = tkinter.Tk()
-    gui_chess_board = GUI.Chess_Board_Frame(window)
-    gui_chess_board.pack()
-    window.mainloop()
