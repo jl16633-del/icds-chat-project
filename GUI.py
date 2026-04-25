206c4271f0356d18bdd80bdac9ed3d20d9efe1e2
jx2924-ux <jx2924@nyu.edu>
2026-04-25 16:50:54 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,69 +2,91 @@ import tkinter
 import math
 import Point
 import Record
+import random
 
 class Chess_Board_Canvas(tkinter.Canvas):
-    def __init__(self, master=None, height=0, width=0):
-        tkinter.Canvas.__init__(self, master, height=height, width=width)
+    def __init__(self, master=None, height=500, width=500, network_client=None):
+        tkinter.Canvas.__init__(self, master, height=height, width=width, bg='#f0d9b5')
+        self.network_client = network_client 
         self.step_record_chess_board = Record.Step_Record_Chess_Board()
         self.init_chess_board_points()    
         self.init_chess_board_canvas() 
+        self.bind('<Button-1>', self.click_canvas)
 
     def init_chess_board_points(self):
         self.chess_board_points = [[None for i in range(15)] for j in range(15)]
-
         for i in range(15):
             for j in range(15):
-                self.chess_board_points[i][j] = Point.Point(i, j);
+                self.chess_board_points[i][j] = Point.Point(i, j)
 
     def init_chess_board_canvas(self):
         for i in range(15): 
-            self.create_line(self.chess_board_points[i][0].pixel_x, self.chess_board_points[i][0].pixel_y, self.chess_board_points[i][14].pixel_x, self.chess_board_points[i][14].pixel_y)
-
+            p1 = self.chess_board_points[i][0]
+            p2 = self.chess_board_points[i][14]
+            self.create_line(p1.pixel_x, p1.pixel_y, p2.pixel_x, p2.pixel_y)
         for j in range(15): 
-            self.create_line(self.chess_board_points[0][j].pixel_x, self.chess_board_points[0][j].pixel_y, self.chess_board_points[14][j].pixel_x, self.chess_board_points[14][j].pixel_y)
+            p1 = self.chess_board_points[0][j]
+            p2 = self.chess_board_points[14][j]
+            self.create_line(p1.pixel_x, p1.pixel_y, p2.pixel_x, p2.pixel_y)
 
-        for i in range(15): 
-            for j in range(15):
-                r = 1
-                self.create_oval(self.chess_board_points[i][j].pixel_x-r, self.chess_board_points[i][j].pixel_y-r, self.chess_board_points[i][j].pixel_x+r, self.chess_board_points[i][j].pixel_y+r);
+    def click_canvas(self, event):
+        i = round((event.x - 30) / 30)
+        j = round((event.y - 30) / 30)
+        
+        if i < 0 or i > 14 or j < 0 or j > 14: return
+        if self.step_record_chess_board.has_record(i, j): return
+        self.draw_remote_move(i, j)
+        frame = self.master.master 
+        if hasattr(frame, 'network_client') and frame.network_client:
+            msg = {"action": "game_move", "location": [i, j]}
+            frame.network_client.send_msg(msg)
+        else:
+            self.after(500, self.ai_move)
+            
+    def execute_move(self, i, j):
+        current_player = self.step_record_chess_board.who_to_play()
+        color = 'white' if current_player == 1 else 'black'
+        
+        p = self.chess_board_points[i][j]
+        self.create_oval(p.pixel_x-10, p.pixel_y-10, p.pixel_x+10, p.pixel_y+10, fill=color)
+                         
+        self.step_record_chess_board.insert_record(i, j)
+        result = self.step_record_chess_board.check()
+        if result == 1 or result == 2:
+            winner = "White" if result == 1 else "Black"
+            self.create_text(250, 480, text=f'{winner} Wins!', font=('Arial', 24, 'bold'), fill='red')
+            self.unbind('<Button-1>')
 
-    def click1(self, event): 
+    def draw_remote_move(self, i, j):
+        if not self.step_record_chess_board.has_record(i, j):
+            self.execute_move(i, j)
+    
+    def ai_move(self):
+        if self.step_record_chess_board.check() in [1, 2]:
+            return
+        empty_spots = []
         for i in range(15):
             for j in range(15):
-                square_distance = math.pow((event.x - self.chess_board_points[i][j].pixel_x), 2) + math.pow((event.y - self.chess_board_points[i][j].pixel_y), 2)
-
-                if (square_distance <= 200) and (not self.step_record_chess_board.has_record(i, j)): #距离小于14并且没有落子
-                    if self.step_record_chess_board.who_to_play() == 1:
-                        self.create_oval(self.chess_board_points[i][j].pixel_x-10, self.chess_board_points[i][j].pixel_y-10, self.chess_board_points[i][j].pixel_x+10, self.chess_board_points[i][j].pixel_y+10, fill='red')
-
-                    elif self.step_record_chess_board.who_to_play() == 2:
-                        self.create_oval(self.chess_board_points[i][j].pixel_x-10, self.chess_board_points[i][j].pixel_y-10, self.chess_board_points[i][j].pixel_x+10, self.chess_board_points[i][j].pixel_y+10, fill='green')
-
-                    self.step_record_chess_board.insert_record(i, j)
-
-                    result = self.step_record_chess_board.check()
-
-
-                    if result == 1:
-                        self.create_text(240, 550, text='the white wins')
-                        self.unbind('<Button-1>')
-
-                    elif result == 2:
-                        self.create_text(240, 550, text='the black wins')
-                        self.unbind('<Button-1>')
+                if not self.step_record_chess_board.has_record(i, j):
+                    empty_spots.append((i, j))
+        if empty_spots:
+            ai_i, ai_j = random.choice(empty_spots)
+            self.draw_remote_move(ai_i, ai_j)
 
 
 class Chess_Board_Frame(tkinter.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, network_client=None):
         tkinter.Frame.__init__(self, master)
+        self.network_client = network_client 
         self.create_widgets()
 
     def create_widgets(self):
-        self.chess_board_label_frame = tkinter.LabelFrame(self, text="Chess Board", padx=5, pady=5)
-        self.chess_board_canvas = Chess_Board_Canvas(self.chess_board_label_frame, height=600, width=480)
+        self.chess_board_label_frame = tkinter.LabelFrame(self, text="Gomoku Chat Mode", padx=5, pady=5)
+        self.chess_board_label_frame.pack(padx=10, pady=10)
+        self.chess_board_canvas = Chess_Board_Canvas(
+            self.chess_board_label_frame, 
+            height=500, width=500, 
+            network_client=self.network_client
+        )
+        self.chess_board_canvas.pack()
 
-        self.chess_board_canvas.bind('<Button-1>', self.chess_board_canvas.click1)
-
-        self.chess_board_label_frame.pack();
-        self.chess_board_canvas.pack();
