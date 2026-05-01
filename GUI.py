2bb9affac196e7fec17214fde7ddcf4f0a0f7995
jx2924-ux <jx2924@nyu.edu>
2026-05-01 18:51:21 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -52,27 +52,68 @@ class Chess_Board_Canvas(tkinter.Canvas):
                          
         self.step_record_chess_board.insert_record(i, j)
         result = self.step_record_chess_board.check()
+        
         if result == 1 or result == 2:
             winner = "White" if result == 1 else "Black"
-            self.create_text(250, 480, text=f'{winner} Wins!', font=('Arial', 24, 'bold'), fill='red')
+            self.create_text(250, 250, text=f'{winner} Wins!', font=('Arial', 36, 'bold'), fill='red')
             self.unbind('<Button-1>')
+
+    def ai_move(self):
+        if self.step_record_chess_board.check() in [1, 2]:
+            return
+            
+        best_score = -1
+        best_move = None
+
+        for i in range(15):
+            for j in range(15):
+                if not self.step_record_chess_board.has_record(i, j):
+                    score = self.evaluate_spot(i, j)
+                    if score > best_score:
+                        best_score = score
+                        best_move = (i, j)
+                        
+        if best_move:
+            self.draw_remote_move(best_move[0], best_move[1])
+
+    def evaluate_spot(self, r, c):
+        score = 0
+        score += (7 - abs(7 - r)) + (7 - abs(7 - c))
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
+        ai_color = self.step_record_chess_board.who_to_play()
+        player_color = 1 if ai_color == 2 else 2
+
+        for dr, dc in directions:
+
+            ai_consecutive = self.count_consecutive(r, c, dr, dc, ai_color)
+            player_consecutive = self.count_consecutive(r, c, dr, dc, player_color)
+            score += 10 ** ai_consecutive
+            score += (10 ** player_consecutive) * 1.2 
+            
+        return score
+
+    def count_consecutive(self, r, c, dr, dc, color):
+        count = 0
+        for step in range(1, 5):
+            nr, nc = r + dr*step, c + dc*step
+            if 0 <= nr < 15 and 0 <= nc < 15:
+                if self.step_record_chess_board.has_record(nr, nc) and self.step_record_chess_board.records[nr][nc].color == color:
+                    count += 1
+                else: break
+            else: break
+        for step in range(1, 5):
+            nr, nc = r - dr*step, c - dc*step
+            if 0 <= nr < 15 and 0 <= nc < 15:
+                if self.step_record_chess_board.has_record(nr, nc) and self.step_record_chess_board.records[nr][nc].color == color:
+                    count += 1
+                else: break
+            else: break
+            
+        return count
 
     def draw_remote_move(self, i, j):
         if not self.step_record_chess_board.has_record(i, j):
             self.execute_move(i, j)
-    
-    def ai_move(self):
-        if self.step_record_chess_board.check() in [1, 2]:
-            return
-        empty_spots = []
-        for i in range(15):
-            for j in range(15):
-                if not self.step_record_chess_board.has_record(i, j):
-                    empty_spots.append((i, j))
-        if empty_spots:
-            ai_i, ai_j = random.choice(empty_spots)
-            self.draw_remote_move(ai_i, ai_j)
-
 
 class Chess_Board_Frame(tkinter.Frame):
     def __init__(self, master=None, network_client=None):
