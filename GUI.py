dbd15420ba73ceb4a799be4868e6239af89821b2
jx2924-ux <jx2924@nyu.edu>
2026-05-02 17:15:53 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -5,9 +5,10 @@ import Record
 import random
 
 class Chess_Board_Canvas(tkinter.Canvas):
-    def __init__(self, master=None, height=500, width=500, network_client=None):
+    def __init__(self, master=None, height=500, width=500, network_client=None, is_multiplayer=True):
         tkinter.Canvas.__init__(self, master, height=height, width=width, bg='#f0d9b5')
         self.network_client = network_client 
+        self.is_multiplayer = is_multiplayer
         self.step_record_chess_board = Record.Step_Record_Chess_Board()
         self.init_chess_board_points()    
         self.init_chess_board_canvas() 
@@ -37,12 +38,13 @@ class Chess_Board_Canvas(tkinter.Canvas):
         if self.step_record_chess_board.has_record(i, j): return
         self.draw_remote_move(i, j)
         frame = self.master.master 
-        if hasattr(frame, 'network_client') and frame.network_client:
+
+        if self.is_multiplayer and hasattr(frame, 'network_client') and frame.network_client:
             msg = {"action": "game_move", "location": [i, j]}
             frame.network_client.send_msg(msg)
         else:
             self.after(500, self.ai_move)
-            
+    
     def execute_move(self, i, j):
         current_player = self.step_record_chess_board.who_to_play()
         color = 'white' if current_player == 1 else 'black'
@@ -57,6 +59,9 @@ class Chess_Board_Canvas(tkinter.Canvas):
             winner = "White" if result == 1 else "Black"
             self.create_text(250, 250, text=f'{winner} Wins!', font=('Arial', 36, 'bold'), fill='red')
             self.unbind('<Button-1>')
+
+            if result == 1 and not self.is_multiplayer and self.network_client:
+                self.network_client.send_msg({"action": "report_win"})
 
     def ai_move(self):
         if self.step_record_chess_board.check() in [1, 2]:
@@ -114,11 +119,12 @@ class Chess_Board_Canvas(tkinter.Canvas):
     def draw_remote_move(self, i, j):
         if not self.step_record_chess_board.has_record(i, j):
             self.execute_move(i, j)
-
+    
 class Chess_Board_Frame(tkinter.Frame):
-    def __init__(self, master=None, network_client=None):
+    def __init__(self, master=None, network_client=None, is_multiplayer=True):
         tkinter.Frame.__init__(self, master)
         self.network_client = network_client 
+        self.is_multiplayer = is_multiplayer
         self.create_widgets()
 
     def create_widgets(self):
@@ -127,7 +133,8 @@ class Chess_Board_Frame(tkinter.Frame):
         self.chess_board_canvas = Chess_Board_Canvas(
             self.chess_board_label_frame, 
             height=500, width=500, 
-            network_client=self.network_client
+            network_client=self.network_client,
+            is_multiplayer=self.is_multiplayer
         )
         self.chess_board_canvas.pack()
 
